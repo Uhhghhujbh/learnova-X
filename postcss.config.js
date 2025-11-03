73b14394be559e8386317ee0ea8e362e7f239775
maxim010 <140669557+Uhhghhujbh@users.noreply.github.com>
2025-11-03 14:21:01 -0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
-import tailwindcss from '@tailwindcss/postcss';
-
 export default {
   plugins: {
-    'postcss-import': {},
-    'postcss-nesting': {},
-    '@tailwindcss/postcss': {},
-    'postcss-custom-properties': {
-      preserve: false
-    },
-    autoprefixer: {
-      flexbox: 'no-2009',
-      grid: 'autoplace'
-    }
-  }
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
